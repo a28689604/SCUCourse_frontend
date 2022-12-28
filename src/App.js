736d03ad2e4ac95060a0a67a8c9a004fa5ee484b
ericcchen <a28689604@gmail.com>
2022-12-28 10:03:42 +0800

    remove console.log

--- src/App.js
+++ src/App.js
@@ -47,10 +47,6 @@ const App = () => {
     });
   }, [analytics]);
 
-  useEffect(() => {
-    console.log(window.location.pathname + window.location.search);
-  });
-
   let routes;
 
   if (token) {
